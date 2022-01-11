be1da9c80b993a19a2251af7cf9e90aa290c0da9
Venkatesh <viv7012@gmail.com>
2022-01-11 14:51:25 +0530

    Solution for Welcome to java problem.

---
+++ src/com/java/solution/WelcomeToJava.java
@@ -0,0 +1,10 @@
+package com.java.solution;
+
+public class WelcomeToJava {
+	  public static void main(String[] args) {
+	        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+	        
+	        System.out.println("Hello, World.");
+	        System.out.println("Hello, Java.");
+	    }
+}
